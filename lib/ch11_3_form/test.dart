bacbbda3b38beabd1764b426386047cce7213945
cheolseung <pre.cheolseung@gmail.com>
2026-09-07 21:49:53 +0900

    Chapter 11-3 Practice

---
+++ lib/ch11_3_form/test.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test'),
+        ),
+        body: TestScreen()
+      ),
+    );
+  }
+}
+
+class TestScreen extends StatefulWidget {
+  @override
+  MyFormState createState() => MyFormState();
+}
+
+class MyFormState extends State<TestScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String? firstName;
+  String? lastName;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: [
+        Text('Form Test'),
+        Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'First Name'
+                ),
+                validator: (value) {
+                  if (value?.isEmpty ?? false) {
+                    return 'Please enter first name.';
+                  }
+                  return null;
+                },
+                onSaved: (String? value) {
+                  firstName = value;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Last Name'
+                ),
+                validator: (value) {
+                  if (value?.isEmpty ?? false) {
+                    return 'Please enter last name.';
+                  }
+                  return null;
+                },
+                onSaved: (String? value) {
+                  lastName = value;
+                },
+              ),
+            ],
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (_formKey.currentState?.validate() ?? false) {
+              _formKey.currentState?.save();
+              print('First Name : $firstName, Last Name : $lastName');
+            }
+          },
+          child: Text('Submit')
+        ),
+      ],
+    );
+  }
+}
