a4f2ec8e3b7ed3e3b894b12270dc645eddd824c2
cheolseung <pre.cheolseung@gmail.com>
2026-09-13 21:53:20 +0900

    Chapter 18-1-1 Practice

---
+++ lib/ch18_1_1_parent_state/test.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+// void main() => runApp(ParentWidget());
+void main() {
+  runApp(ParentWidget());
+}
+
+class ParentWidget extends StatefulWidget {
+  @override
+  ParentWidgetState createState() => ParentWidgetState();
+}
+
+class ParentWidgetState extends State<ParentWidget> {
+  bool favorited = false;
+  int favoriteCount = 10;
+
+  void toggleFavorite() {
+    setState(() {
+      if (favorited) {
+        favoriteCount -= 1;
+        favorited = false;
+      } else {
+        favoriteCount += 1;
+        favorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconWidget(
+              favorited: favorited,
+              onChanged: toggleFavorite
+            ),
+            ContentWidget(favoriteCount: favoriteCount)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class IconWidget extends StatelessWidget {
+  final bool favorited;
+  final Function onChanged;
+
+  IconWidget({this.favorited = false, required this.onChanged});
+
+  void _handleTap() {
+    onChanged();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: IconButton(
+        onPressed: _handleTap,
+        icon: (favorited ? Icon(Icons.favorite) : Icon(Icons.favorite_border)),
+        iconSize: 200,
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class ContentWidget extends StatelessWidget {
+  final int favoriteCount;
+
+  ContentWidget({required this.favoriteCount});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: Text(
+            'Favorite Count : $favoriteCount',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
