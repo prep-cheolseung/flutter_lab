6457a04aecf02bdf24a1b01d2e0c65d535874cc4
cheolseung <pre.cheolseung@gmail.com>
2026-09-20 23:51:38 +0900

    Chapter 23-1 Practice

---
+++ lib/ch23_1_geolocator/test.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+// void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      theme: new ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: NativePluginWidget(),
+    );
+  }
+}
+
+class NativePluginWidget extends StatefulWidget {
+  @override
+  NativePluginWidgetState createState() => NativePluginWidgetState();
+}
+
+class NativePluginWidgetState extends State<NativePluginWidget> {
+  String? latitude;
+  String? longitude;
+
+  getGeoData() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Permissions are denied');
+      }
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      latitude = position.latitude.toString();
+      longitude = position.longitude.toString();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getGeoData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Geolocator"),
+      ),
+      body: Container(
+        color: Colors.indigo,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'MyLocation',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                'Latitude : ${latitude}',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                'Longitude : ${longitude}',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
